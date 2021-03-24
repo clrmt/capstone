e91b67f40de6fb3fb3ec8d6317f634c6ab5ecdc5
clrmt <clrmt@naver.com>
2021-03-25 03:01:36 +0900

    adjust parameters / change weight save file

--- agent.py
+++ agent.py
@@ -12,33 +12,35 @@ from keras.optimizers import Adam
 #Env(True)
 
 gamma = 0.95
-
 epsilon = 1.0
 
 env = gym.make('CartPole-v1')
 
-memory = deque(maxlen=1024)
+memory = deque(maxlen=2048)
 
 # create Network
 model = Sequential()
 model.add(Dense(64, input_dim=4, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(2, activation='linear'))
-model.compile(loss='mse', optimizer=Adam(lr=0.002))
-if os.path.isfile("save.chkpt"):
-    model.load_weights("save.chkpt")
+model.compile(loss='mse', optimizer=Adam(lr=0.001))
+if os.path.isfile("save.h5"):
+    model.load_weights("save.h5")
 
 for episodeNumber in range(500):
     state = env.reset()
     state = np.reshape(state, [1, 4])
 
+    maxFrame = 0
+
     done = False
     for frameCounter in range(500):
-        env.render()
+        #env.render()
 
         if np.random.rand() <= epsilon:
-            action = random.randrange(4)
-        action = np.argmax(model.predict(state)[0])
+            action = random.randrange(2)
+        else :
+            action = np.argmax(model.predict(state)[0])
 
         next_state, reward, done, _ = env.step(action)
         next_state = np.reshape(next_state, [1, 4])
@@ -47,10 +49,14 @@ for episodeNumber in range(500):
 
         state = next_state
 
+        maxFrame = frameCounter
+
         if done == True:
             break
 
-    # 저장된 데이터로부터 학습(복기)
+    print("에피소드: ", episodeNumber, ": ", maxFrame)
+
+    # replay memory
     if len(memory) >= 64:
         sample = random.sample(memory, 64)
         for state, action, reward, next_state, done in sample:
@@ -60,7 +66,15 @@ for episodeNumber in range(500):
             target_f = model.predict(state)
             target_f[0][action] = target
             model.fit(state, target_f, epochs=1, verbose=0)
-
+    '''
+    for state, action, reward, next_state, done in memory:
+        target = reward
+        if not done:
+            target = reward + gamma * np.amax(model.predict(next_state)[0])
+        target_f = model.predict(state)
+        target_f[0][action] = target
+        model.fit(state, target_f, epochs=1, verbose=0)
+    '''        
     # 랜덤으로 선택할 확률(e-greedy)
     epsilon *= 0.995
     if epsilon < 0.02:
@@ -68,4 +82,4 @@ for episodeNumber in range(500):
 
     # 때때로 파일로 저장
     if episodeNumber % 10 == 9:
-        model.save("save.chkpt")
+        model.save("save.h5")
