f674e439fc7a6a159e51fd19bceba0b49f0a7c0f
clrmt <clrmt@naver.com>
2021-04-05 06:30:03 +0900

    improved performance with GradientTape

--- agent.py
+++ agent.py
@@ -2,6 +2,7 @@ import os
 import random
 import numpy as np
 from collections import deque
+import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
@@ -19,11 +20,13 @@ model = Sequential()
 model.add(Dense(64, input_dim=inputDim, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(outputDim))
-model.compile(loss='mse', optimizer=Adam(lr=learningRate))
+opt = Adam(lr=learningRate)
+#model.compile(loss='mse', optimizer=Adam(lr=learningRate))
+
 if os.path.isfile("save.h5"):
     model.load_weights("save.h5")
 
-for episodeNumber in range(500):
+for episodeNumber in range(episodeNumber):
     state = env.reset()
     state = np.reshape(state, [1, inputDim])
 
@@ -31,13 +34,15 @@ for episodeNumber in range(500):
     totalReward = 0
 
     done = False
+    
     for frameCounter in range(500):
         env.render()
+        sleep(0.03)
 
         if np.random.rand() <= epsilon:
             action = random.randrange(outputDim)
         else :
-            action = np.argmax(model.predict(state)[0])
+            action = np.argmax(model(state)[0])
 
         next_state, reward, done, _ = env.step(action)
         next_state = np.reshape(next_state, [1, inputDim])
@@ -56,17 +61,23 @@ for episodeNumber in range(500):
 
     # replay memory
     if len(memory) >= replaySize:
-        random.shuffle(memory)
 
-        for i in range(replaySize):
-            state, action, reward, next_state, done = memory[i]
-            target = reward
-            if not done:
-                target = reward + gamma * np.amax(model.predict(next_state)[0])
-            target_f = model.predict(state)
-            target_f[0][action] = target
+        mini_batch = random.sample(memory, replaySize)
 
-            model.fit(state, target_f, epochs=1, verbose=0) # gradient 자동 계산(매우 느림) -> 나중에 개선
+        states = np.array([sample[0][0] for sample in mini_batch])
+        actions = np.array([sample[1] for sample in mini_batch])
+        rewards = np.array([sample[2] for sample in mini_batch])
+        next_states = np.array([sample[3][0] for sample in mini_batch])
+        dones = np.array([sample[4] for sample in mini_batch])
+
+        with tf.GradientTape() as tape:
+            
+            predicts = tf.reduce_sum(tf.one_hot(actions, outputDim) * model(states), axis = 1)
+            targets = rewards + (1 - dones) * gamma * np.amax(model(next_states), axis = -1)
+            loss = tf.reduce_mean(tf.square(targets - predicts))
+            
+        grads = tape.gradient(loss, model.trainable_variables)
+        opt.apply_gradients(zip(grads, model.trainable_variables))
 
     # 랜덤으로 선택할 확률(e-greedy)
     epsilon *= epsilonDecay
@@ -74,5 +85,5 @@ for episodeNumber in range(500):
         epsilon = epsilonMin
 
     # 때때로 파일로 저장
-    if episodeNumber % 10 == 9:
+    if episodeNumber % 20 == 9:
         model.save("save.h5")
--- hyperparameters.py
+++ hyperparameters.py
@@ -2,9 +2,10 @@ gamma = 0.95
 learningRate = 0.0005
 
 # only DQN
+episodeNumber = 1000
 bufferSize = 2048
 replaySize = 64
-epsilon = 1.0
+epsilon = 0.0
 epsilonDecay = 0.997
 epsilonMin = 0.05
 
